ebd4391fb42f257e38a9aea117fcc172f3a81e9f
andrescol24 <andresmil24@hotmail.com>
2023-08-31 21:26:51 -0500

    added method to read text template and its parameter names

--- src/main/java/com/epam/ld/module2/testing/template/Template.java
+++ src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -1,6 +1,7 @@
 package com.epam.ld.module2.testing.template;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -13,13 +14,23 @@ public class Template {
 
     }
     public Template(String text) {
-
+        this.text = text;
+        this.parameterNames = readParameters(text);
     }
     public Set<String> getParameterNames() {
         return parameterNames;
     }
-
     public String getText() {
         return text;
+    }
+    private static Set<String> readParameters(String text) {
+        Set<String> parameterNames = new HashSet<>();
+        for (String word : text.split(" ")) {
+            if(word.length() > 3 && word.startsWith("#{") && word.endsWith("}")) {
+                String parameterName = word.substring(2, word.length() - 2);
+                parameterNames.add(parameterName);
+            }
+        }
+        return parameterNames;
     }
 }
--- src/test/java/com/epam/ld/module2/testing/template/TemplateTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateTest.java
@@ -2,15 +2,30 @@ package com.epam.ld.module2.testing.template;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class TemplateTest {
 
     @Test
-    public void templateTextTest() {
+    public void textTemplateHasExpectedTextTest() {
         String text = "<h1>Welcome #{name} to our #{projectName} project</h1>";
         Template template = new Template(text);
         assertEquals(text, template.getText());
+    }
+    @Test
+    public void textTemplateHasExpectedParametersTest() {
+        String text = "<h1>Welcome #{name} to our #{projectName} project</h1>";
+        Set<String> expectedParameters = new HashSet<>(Arrays.asList("name", "projectName"));
+
+        Template template = new Template(text);
+
+        assertEquals(expectedParameters, template.getParameterNames());
     }
 }
