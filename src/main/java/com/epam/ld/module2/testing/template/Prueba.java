387923fe0b7111d0dffe8cd21b8a83915388503c
andrescol24 <andresmil24@hotmail.com>
2023-09-01 21:08:58 -0500

    Agrege nueva clase

--- src/main/java/com/epam/ld/module2/testing/template/Prueba.java
+++ src/main/java/com/epam/ld/module2/testing/template/Prueba.java
@@ -1,2 +1,10 @@
-package com.epam.ld.module2.testing.template;public class Prueba {
+package com.epam.ld.module2.testing.template;
+
+public class Prueba {
+    public static void main(String[] args) {
+
+        // String es una cadena de text
+        float valorTotal = 12000;
+        System.out.println("Mi nombre es " + valorTotal);
+    }
 }
